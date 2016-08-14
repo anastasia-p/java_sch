ffc0f572b36152d0987faff1514354900b6be414
anastasia-p <anastasia.aap@gmail.com>
2016-08-14 22:55:54 +0300

    Code style settings were changed

--- sandbox/src/main/java/ru/stqa/sch/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/sch/sandbox/MyFirstProgram.java
@@ -1,7 +1,7 @@
 package ru.stqa.sch.sandbox;
 
 public class MyFirstProgram {
-	public static void main(String[] args) {
-		System.out.print("Hello, world!");
-	}
+  public static void main(String[] args) {
+    System.out.print("Hello, world!");
+  }
 }
