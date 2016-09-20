4d5550373b11c9fe200ab2b59cc47b797613bf80
anastasia-p <anastasia.aap@gmail.com>
2016-09-20 21:47:54 +0300

    test deletion was renamed

--- addressbook-web-tests/src/test/java/ru/stqa/sch/addressbook/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/sch/addressbook/GroupDeletionTests.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 public class GroupDeletionTests extends TestBase{
 
     @Test
-    public void GroupDeletionTests() {
+    public void testGroupDeletion() {
         gotoGroupPage();
         selectGroup();
         deleteSelectedGroups();
