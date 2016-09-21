f21dd976d9c4eb652d085c8f328920e38b97f195
anastasia-p <anastasia.aap@gmail.com>
2016-09-21 14:58:51 +0300

    contact modification test was added

--- addressbook-web-tests/src/test/java/ru/stqa/sch/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/sch/addressbook/appmanager/ContactHelper.java
@@ -31,4 +31,11 @@ public class ContactHelper extends HelperBase{
         click(By.xpath("//div[@id='content']/form[2]/div[2]/input"));
     }
 
+    public void initContactModification() {
+        click(By.xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img"));
+    }
+    public void submitContactModification() {
+        click(By.name("update"));
+    }
+
 }
---
+++ addressbook-web-tests/src/test/java/ru/stqa/sch/addressbook/tests/ModificationTests.java
@@ -0,0 +1,29 @@
+package ru.stqa.sch.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.sch.addressbook.model.ContactData;
+import ru.stqa.sch.addressbook.model.GroupData;
+
+public class ModificationTests extends TestBase{
+
+    @Test
+    public void testGroupModificztion() {
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("test2", "header2", "footer2"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+    }
+
+    @Test
+    public void testContactModification() {
+        app.getNavigationHelper().gotoHomePage();
+        app.getContactHelper().selectContact();
+        app.getContactHelper().initContactModification();
+        app.getContactHelper().fillContactForm(new ContactData("firstName3", "lastName3", "address3", "234234",
+                "test3@test.ru"));
+        app.getContactHelper().submitContactModification();
+        app.getNavigationHelper().gotoHomePage();
+    }
+}
