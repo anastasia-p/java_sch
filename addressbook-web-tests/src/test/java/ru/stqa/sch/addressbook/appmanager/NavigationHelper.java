19a44ad3244decb89b900176a7bad297897e284d
anastasia-p <anastasia.aap@gmail.com>
2016-09-24 13:07:21 +0300

    right link check

--- addressbook-web-tests/src/test/java/ru/stqa/sch/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/sch/addressbook/appmanager/NavigationHelper.java
@@ -12,14 +12,26 @@ public class NavigationHelper extends HelperBase{
     }
 
     public void gotoContactPage() {
+        if (isElementPresence(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Edit / add address book entry")) {
+            return;
+        }
         click(By.linkText("add new"));
     }
 
     public void gotoGroupPage() {
+        if (isElementPresence(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresence(By.name("new"))) {
+            return;
+        }
         click(By.linkText("groups"));
     }
 
     public void gotoHomePage() {
+        if (isElementPresence(By.id("maintable"))) {
+            return;
+        }
         click(By.linkText("home"));
     }
 }
