725f7940f5e6e8b965c3ceaccf25a4f869472afa
anastasia-p <anastasia.aap@gmail.com>
2016-10-13 22:31:37 +0300

    lambda

--- addressbook-web-tests/src/test/java/ru/stqa/sch/addressbook/tests/CreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/sch/addressbook/tests/CreationTests.java
@@ -1,6 +1,5 @@
 package ru.stqa.sch.addressbook.tests;
 
-import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.sch.addressbook.model.ContactData;
@@ -11,47 +10,35 @@ import java.util.List;
 
 public class CreationTests extends TestBase {
 
-    @Test
-    public void testGroupCreation() {
-        app.getNavigationHelper().gotoGroupPage();
-        List<GroupData> before = app.getGroupHelper().getGroupList();
-        GroupData group = new GroupData("test1", null, null);
-        app.getGroupHelper().createGroup(group);
-        List<GroupData> after = app.getGroupHelper().getGroupList();
-        Assert.assertEquals(after.size(), before.size() + 1);
+  @Test
+  public void testGroupCreation() {
+    app.getNavigationHelper().gotoGroupPage();
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    GroupData group = new GroupData("test1", null, null);
+    app.getGroupHelper().createGroup(group);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() + 1);
 
-        int max = 0;
-        for (GroupData q : after) {
-            if (q.getId() > max) {
-                max = q.getId();
-            }
-        }
-        group.setId(max);
-        before.add(group);
-        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
-    }
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+  }
 
-    @Test
-    public void testContactCreation() {
-        app.getNavigationHelper().gotoHomePage();
-        List<ContactData> before = app.getContactHelper().getContactList();
-        app.getNavigationHelper().gotoContactPage();
-        ContactData contact = new ContactData("firstName2", "lastName2", "address2",
-                "123123", "test2@test.ru", "test1");
-        app.getContactHelper().createContact(contact, true);
-        app.getNavigationHelper().gotoHomePage();
-        List<ContactData> after = app.getContactHelper().getContactList();
-        Assert.assertEquals(after.size(), before.size() + 1);
+  @Test
+  public void testContactCreation() {
+    app.getNavigationHelper().gotoHomePage();
+    List<ContactData> before = app.getContactHelper().getContactList();
+    app.getNavigationHelper().gotoContactPage();
+    ContactData contact = new ContactData("firstName2", "lastName2", "address2",
+            "123123", "test2@test.ru", "test1");
+    app.getContactHelper().createContact(contact, true);
+    app.getNavigationHelper().gotoHomePage();
+    List<ContactData> after = app.getContactHelper().getContactList();
+    Assert.assertEquals(after.size(), before.size() + 1);
 
-      int max = 0;
-      for (ContactData q : after) {
-        if (q.getId() > max) {
-          max = q.getId();
-        }
-      }
-      contact.setId(max);
-      before.add(contact);
-      Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
-    }
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+    before.add(contact);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+  }
 
 }
