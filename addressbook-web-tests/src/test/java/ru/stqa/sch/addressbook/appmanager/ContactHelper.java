27868e912e1126a7ff182f9921d77a2816b3ffe2
anastasia-p <anastasia.aap@gmail.com>
2016-10-15 21:29:39 +0300

    contact address test was added

--- addressbook-web-tests/src/test/java/ru/stqa/sch/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/sch/addressbook/appmanager/ContactHelper.java
@@ -103,9 +103,10 @@ public class ContactHelper extends HelperBase{
             int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
             String lastName = element.findElement(By.xpath("//td[2]")).getText();
             String firstName = element.findElement(By.xpath("//td[3]")).getText();
+            String address = element.findElement(By.xpath("//td[4]")).getText();
             String allPhones = element.findElement(By.xpath("//td[6]")).getText();
             contactCache.add(new ContactData().withId(id).withFirstname(firstName).withLastname(lastName)
-                    .withAllPhones(allPhones));
+                    .withAllPhones(allPhones).withAddress(address));
         }
         return new Contacts(contactCache);
     }
@@ -117,9 +118,10 @@ public class ContactHelper extends HelperBase{
         String home = wd.findElement(By.name("home")).getAttribute("value");
         String mobile = wd.findElement(By.name("mobile")).getAttribute("value");
         String work = wd.findElement(By.name("work")).getAttribute("value");
+        String address = wd.findElement(By.name("address")).getAttribute("value");
         wd.navigate().back();
         return new ContactData().withId(contact.getId()).withFirstname(firstname).withLastname(lastname)
-                .withHomePhone(home).withMobilePhone(mobile).withWorkPhone(work);
+                .withHomePhone(home).withMobilePhone(mobile).withWorkPhone(work).withAddress(address);
     }
 
     private void initContactModificationById(int id) {
---
+++ addressbook-web-tests/src/test/java/ru/stqa/sch/addressbook/tests/ContactAddressTest.java
@@ -0,0 +1,26 @@
+package ru.stqa.sch.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.sch.addressbook.model.ContactData;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ContactAddressTest extends TestBase {
+
+  @Test
+  public void testContactAddress() {
+    app.contact().checkContact(new ContactData()
+            .withFirstname("firstName2").withLastname("lastName2")
+            .withAddress("address2   test"), true);
+    app.goTo().homePage();
+    ContactData contact = app.contact().all().iterator().next();
+    ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+
+    assertThat(cleaned(contact.getAddress()), equalTo(cleaned(contactInfoFromEditForm.getAddress())));
+  }
+
+  public static String cleaned(String phone) {
+    return phone.replaceAll(" ", "");
+  }
+}
