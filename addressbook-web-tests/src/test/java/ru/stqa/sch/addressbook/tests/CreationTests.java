833dd39ab1b34f24434a20a6cb6a2c99f361540f
anastasia-p <anastasia.aap@gmail.com>
2016-10-18 20:01:25 +0300

    xml format for groups was added

--- addressbook-web-tests/src/test/java/ru/stqa/sch/addressbook/tests/CreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/sch/addressbook/tests/CreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.sch.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.sch.addressbook.model.ContactData;
@@ -11,6 +12,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
@@ -19,15 +21,17 @@ public class CreationTests extends TestBase {
 
   @DataProvider
   public Iterator<Object[]> validGroups() throws IOException {
-    List<Object[]> list = new ArrayList<Object[]>();
-    BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/groups.csv"));
+    BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/groups.xml"));
+    String xml = "";
     String line = reader.readLine();
     while (line != null) {
-      String[] split = line.split(";");
-      list.add(new Object[]{new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
-      line = reader.readLine();
+      xml += line;
+       line = reader.readLine();
     }
-    return list.iterator();
+    XStream xstream = new XStream();
+    xstream.processAnnotations(GroupData.class);
+    List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
+    return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
   }
 
   @DataProvider
