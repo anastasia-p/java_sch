7dc9b03f874fe5d48b448434a0b8b4e7c2aa87ed
anastasia-p <anastasia.aap@gmail.com>
2016-10-19 16:39:32 +0300

    GroupModification db

--- addressbook-web-tests/src/test/java/ru/stqa/sch/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/sch/addressbook/appmanager/ApplicationManager.java
@@ -26,6 +26,7 @@ public class ApplicationManager {
     private GroupHelper groupHelper;
     private HelperBase helperBase;
     private String browser;
+    private DbHelper dbHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -35,6 +36,8 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+        dbHelper = new DbHelper();
 
         if (browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
@@ -72,5 +75,9 @@ public class ApplicationManager {
 
     public HelperBase getHelperBase() {
         return helperBase;
+    }
+
+    public DbHelper db() {
+        return dbHelper;
     }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/sch/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/sch/addressbook/tests/GroupModificationTest.java
@@ -1,5 +1,6 @@
 package ru.stqa.sch.addressbook.tests;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.sch.addressbook.model.GroupData;
 import ru.stqa.sch.addressbook.model.Groups;
@@ -9,16 +10,23 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupModificationTest extends TestBase{
 
+    @BeforeMethod
+    public void ensurePreconditions() {
+        if (app.db().groups().size() == 0) {
+            app.group().checkGroup(new GroupData().withName("test1"));
+        }
+    }
+
     @Test
     public void testGroupModification() {
-        app.group().checkGroup(new GroupData().withName("test1"));
-        Groups before = app.group().all();
+        Groups before = app.db().groups();
         GroupData modifiedGroup = before.iterator().next();
         GroupData group = new GroupData()
                 .withId(modifiedGroup.getId()).withName("test2").withHeader("header2").withFooter("footer2");
+        app.goTo().groupPage();
         app.group().modify(group);
         assertThat(app.group().count(), equalTo(before.size()));
-        Groups after = app.group().all();
+        Groups after = app.db().groups();
         assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
     }
 }
