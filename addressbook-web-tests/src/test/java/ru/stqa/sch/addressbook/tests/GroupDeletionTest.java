96acacc2d1cf38e5aa10211905e5082471843c3f
anastasia-p <anastasia.aap@gmail.com>
2016-10-19 16:44:28 +0300

    GroupDeletion db

--- addressbook-web-tests/src/test/java/ru/stqa/sch/addressbook/tests/GroupDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/sch/addressbook/tests/GroupDeletionTest.java
@@ -1,5 +1,6 @@
 package ru.stqa.sch.addressbook.tests;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.sch.addressbook.model.GroupData;
 import ru.stqa.sch.addressbook.model.Groups;
@@ -9,14 +10,21 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupDeletionTest extends TestBase{
 
+    @BeforeMethod
+    public void ensurePreconditions() {
+        if (app.db().groups().size() == 0) {
+            app.group().checkGroup(new GroupData().withName("test1"));
+        }
+    }
+
     @Test
     public void testGroupDeletion() {
-        app.group().checkGroup(new GroupData().withName("test1"));
-        Groups before = app.group().all();
+        Groups before = app.db().groups();
+        app.goTo().groupPage();
         GroupData deletedGroup = before.iterator().next();
         app.group().delete(deletedGroup);
         assertThat(app.group().count(), equalTo(before.size() - 1));
-        Groups after = app.group().all();
+        Groups after = app.db().groups();
         assertThat(after, equalTo(before.without(deletedGroup)));
     }
 }
