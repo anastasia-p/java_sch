297d1af36197d346ca61a9e4b5f713f1b8b1258a
anastasia-p <anastasia.aap@gmail.com>
2016-10-19 17:16:23 +0300

    ContactDeletion db

--- addressbook-web-tests/src/test/java/ru/stqa/sch/addressbook/tests/ContactDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/sch/addressbook/tests/ContactDeletionTest.java
@@ -1,5 +1,6 @@
 package ru.stqa.sch.addressbook.tests;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.sch.addressbook.model.ContactData;
 import ru.stqa.sch.addressbook.model.Contacts;
@@ -11,17 +12,23 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactDeletionTest extends TestBase{
 
+    @BeforeMethod
+    public void ensurePreconditions() {
+        if (app.db().contacts().size() == 0) {
+            app.contact().checkContact(new ContactData()
+                    .withFirstname("firstName2").withLastname("lastName2").withAddress("address2")
+                    .withMobilePhone("123123").withEmail("test2@test.ru"), true);
+        }
+    }
+
     @Test
     public void testContactDeletion() {
-        app.contact().checkContact(new ContactData()
-                .withFirstname("firstName2").withLastname("lastName2").withAddress("address2")
-                .withMobilePhone("123123").withEmail("test2@test.ru"), true);
+        Contacts before = app.db().contacts();
         app.goTo().homePage();
-        Contacts before = app.contact().all();
         ContactData deletedContact = before.iterator().next();
         app.contact().delete(deletedContact);
         assertThat(app.contact().count(), equalTo(before.size() - 1));
-        Contacts after = app.contact().all();
+        Contacts after = app.db().contacts();
         assertThat(after, equalTo(before.without(deletedContact)));
     }
 }
